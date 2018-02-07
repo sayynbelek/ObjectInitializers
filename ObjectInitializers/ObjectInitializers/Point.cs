225fa64110e8fa54c7502ebfdad0ad5d63a4f431
СайынБелек Алексеевич Кошкароол <sakoshkarool@ttit.local>
2018-02-07 09:15:06 +0700

    Add

---
+++ ObjectInitializers/ObjectInitializers/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int xVal, int yVal)
+        {
+            X = xVal;
+            Y = yVal;
+        }
+        public Point() { }
+        public void DisplayStats()
+        {
+            Console.WriteLine("[{0}, {1}]", X, Y);
+        }
+    }
+}
---
+++ ObjectInitializers/ObjectInitializers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Object Init Syntax *****\n");
+
+            // Создать объект Point с установкой каждого свойства вручную
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            // Создать объект Point с использованием специального конструктора
+            Point anotherPoint = new Point();
+            anotherPoint.DisplayStats();
+
+            // Создать объект Point с использованием синтаксиса инициализатора объекта
+            Point finalPoint = new Point { X = 30, Y = 30 };
+            finalPoint.DisplayStats();
+            Console.ReadLine();
+        }
+    }
+}
